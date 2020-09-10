9aebca9f8f4200e7517a4bc1b788acf52eea1ac6
Lea Dalibard <leadalibard@gmail.com>
2020-09-10 17:07:18 +0200

    Saving updates => Making a random array so that images are distributed randomkly

--- script.js
+++ script.js
@@ -9,6 +9,18 @@
         "images/pikachu.png"
     ];
 
+    const randomArray = []
+    for (let i = 0; i < 11; i++) {
+        var randomImg=
+        randomArray.push(Math.floor(Math.random() * 11))
+    }
+    var x = document.getElementsByTagName("td");
+    for (let i = 0; i < length; i++) {
+        x[i].innerHTML = randomArray[i];
+    }
+
+    x.forEach(index=>  x[index].innerHTML = randomArray[index]);
+
         Array.from(document.querySelectorAll("button")).forEach(function ($btn, index) {
             $btn.addEventListener("click", function () {
                 var random = Math.floor(Math.random() * 5);
