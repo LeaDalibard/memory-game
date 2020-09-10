8cfad375e34703ee9dbbac0c56085f5488152244
Lea Dalibard <leadalibard@gmail.com>
2020-09-10 16:21:24 +0200

    Making card turn randomly when turned

--- script.js
+++ script.js
@@ -6,9 +6,16 @@
         "images/flareon.png",
         "images/sandschrew.png",
         "images/squirtle.png",
+        "images/pikachu.png"
     ];
 
+        Array.from(document.querySelectorAll("button")).forEach(function ($btn, index) {
+            $btn.addEventListener("click", function () {
+                var random = Math.floor(Math.random() * 5);
+                document.getElementById("img-"+ index).src = pokemon[random];
+            });
+        });
 
+})
 
-
-})();
+();
