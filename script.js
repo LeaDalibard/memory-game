1530a911c83264b2da80adae34dbef67545a8c74
Lea Dalibard <leadalibard@gmail.com>
2020-09-11 09:48:47 +0200

    Changing 1 image + card return on click to randomly organised images

--- script.js
+++ script.js
@@ -26,21 +26,9 @@
     var randomCard= shuffle(card);
     console.log(randomCard);
 
-    /* for (let i = 0; i < 11; i++) {
-         var randomImg=
-         randomArray.push(Math.floor(Math.random() * 11))
-     }
-     var x = document.getElementsByTagName("td");
-     for (let i = 0; i < length; i++) {
-         x[i].innerHTML = randomArray[i];
-     }
-
-     x.forEach(index=>  x[index].innerHTML = randomArray[index]);*/
-
     Array.from(document.querySelectorAll("button")).forEach(function ($btn, index) {
         $btn.addEventListener("click", function () {
-            var random = Math.floor(Math.random() * 5);
-            document.getElementById("img-" + index).src = pokemon[random];
+            document.getElementById("img-" + index).src = randomCard[index];
         });
     });
 
