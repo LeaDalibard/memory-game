f0d2d5fa10142aa2721f7ae4886e689387450448
Lea Dalibard <leadalibard@gmail.com>
2020-09-11 16:01:23 +0200

    Saving updates before Friiiday games

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 (function () {
 
-        var pokemon = [
-            "images/bulbasaur.jpeg",
-            "images/charmander.png",
-            "images/flareon.png",
-            "images/sandschrew.png",
-            "images/squirtle.png",
-            "images/pikachu.png"
-        ];
-        /*-----------Randomizing images--------------*/
-        var card = pokemon.concat(pokemon);
+    var pokemon = [
+        "images/bulbasaur.jpeg",
+        "images/charmander.png",
+        "images/flareon.png",
+        "images/sandschrew.png",
+        "images/squirtle.png",
+        "images/pikachu.png"
+    ];
+    /*-----------Randomizing images--------------*/
+    var card = pokemon.concat(pokemon);
 
-        function shuffle(a) {
-            var j, x, i;
-            for (i = a.length - 1; i > 0; i--) {
-                j = Math.floor(Math.random() * (i + 1));
-                x = a[i];
-                a[i] = a[j];
-                a[j] = x;
-            }
-            return a;
+    function shuffle(a) {
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
         }
+        return a;
+    }
 
-        var randomCard = shuffle(card);
+    var randomCard = shuffle(card);
+    for (var i = 0; let i < randomCard.length;
+    i++
+)
+    { document.getElementById("imgb-" + i).src = randomCard[i];
+    }
 
-        /*------------Turning images-----------------*/
+    /*------------Turning images-----------------*/
 
-        var count = 1;
+
+})();
+
+/*
+var count = 1;
         Array.from(document.querySelectorAll("button")).forEach(function ($btn, index) {
             $btn.addEventListener("click", function () {
                 document.getElementById("img-" + index).src = randomCard[index];
@@ -58,53 +67,19 @@
             })
 
         }
-
-
-    })();
-
-/*
-
- Array.from(document.querySelectorAll("button")).forEach(function ($btn, index) {
-            $btn.addEventListener("click", function () {
-                count = count + 1;
-                document.getElementById("img-" + index).src = randomCard[index];
-                if (count === 2) {
-                    cardTwo = randomCard[index];
-                } else {
-                    cardOne = randomCard[index];
-                }
-                if (cardOne !== cardTwo) {
-                    function returnCard() {
-                        setTimeout(function () {
-                            document.getElementById("img-" + index).src = "images/pokeball.png";
-                        }, 5000);
-                    };
-                    var count = 0;
-                    cardOne = "";
-                    cardTwo = "";
-                } else {
-                    var count = 0;
-                    cardOne = "";
-                    cardTwo = "";
-                }
-
-            });
-        });
-    }
-)
 */
- /* 1 get the card //
- * 2 loop over them and listen for event => flipping function, get the id of the card
- * 3 create array to check if the same
- * 4 if card1 != card2 turn back else stay
- * 5 counter=1
- * 6 src images
- * 7 creating the flipping function with the parameter card-id
- * 8 in Flipping function => get the card by id// let card = document.getElementById(${imageId}) (can put parameter of the function)
- * 9 generate random index => get random images (srcimg[randomIndex])
- * 10 push random generate images to check array
- * 11 if counter<2 firstcard= display random image, flip it counter++ => and remove from random sources images
- * 12 else if firstcard.src= secondcard.src then display the second
- * 13 else flip both counter-- + firstcard=""+ checkarray=""
- *
- * */
+/* 1 get the card //
+* 2 loop over them and listen for event => flipping function, get the id of the card
+* 3 create array to check if the same
+* 4 if card1 != card2 turn back else stay
+* 5 counter=1
+* 6 src images
+* 7 creating the flipping function with the parameter card-id
+* 8 in Flipping function => get the card by id// let card = document.getElementById(${imageId}) (can put parameter of the function)
+* 9 generate random index => get random images (srcimg[randomIndex])
+* 10 push random generate images to check array
+* 11 if counter<2 firstcard= display random image, flip it counter++ => and remove from random sources images
+* 12 else if firstcard.src= secondcard.src then display the second
+* 13 else flip both counter-- + firstcard=""+ checkarray=""
+*
+* */
