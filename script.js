aa78888d8e3050574f3d26e4f3045c4d187f1dca
Lea Dalibard <leadalibard@gmail.com>
2020-09-11 11:25:26 +0200

    images turning but not turning back

--- script.js
+++ script.js
@@ -1,61 +1,85 @@
 (function () {
 
-    var pokemon = [
-        "images/bulbasaur.jpeg",
-        "images/charmander.png",
-        "images/flareon.png",
-        "images/sandschrew.png",
-        "images/squirtle.png",
-        "images/pikachu.png"
-    ];
-    /*-----------Randomizing images--------------*/
-    var card = pokemon.concat(pokemon);
+        var pokemon = [
+            "images/bulbasaur.jpeg",
+            "images/charmander.png",
+            "images/flareon.png",
+            "images/sandschrew.png",
+            "images/squirtle.png",
+            "images/pikachu.png"
+        ];
+        /*-----------Randomizing images--------------*/
+        var card = pokemon.concat(pokemon);
 
-    function shuffle(a) {
-        var j, x, i;
-        for (i = a.length - 1; i > 0; i--) {
-            j = Math.floor(Math.random() * (i + 1));
-            x = a[i];
-            a[i] = a[j];
-            a[j] = x;
+        function shuffle(a) {
+            var j, x, i;
+            for (i = a.length - 1; i > 0; i--) {
+                j = Math.floor(Math.random() * (i + 1));
+                x = a[i];
+                a[i] = a[j];
+                a[j] = x;
+            }
+            return a;
         }
-        return a;
-    }
 
-    var randomCard = shuffle(card);
+        var randomCard = shuffle(card);
 
-    /*------------Promises to turn images-----------------*/
+        /*------------Turning images-----------------*/
 
-    Array.from(document.querySelectorAll("button")).forEach(function ($btn) {
-        $btn.addEventListener("click", function () {
-            performOperation($btn.id);
+        var count = 0;
+        cardOne = "";
+        cardTwo = "";
+        Array.from(document.querySelectorAll("button")).forEach(function ($btn, index) {
+            $btn.addEventListener("click", function () {
+                count = count + 1;
+                document.getElementById("img-" + index).src = randomCard[index];
+                if (count === 2) {
+                    cardTwo = randomCard[index]
+                } else {
+                    cardOne = randomCard[index]
+                }
+                ;
+                if (cardOne !== cardTwo) {
+                    function returnCard() {
+                        setTimeout(function () {
+                            document.getElementById("img-" + index).src = "images/pokeball.png";
+                        }, 5000);
+                    };
+                    var count = 0;
+                    cardOne = "";
+                    cardTwo = "";
+                } else {
+                    var count = 0;
+                    cardOne = "";
+                    cardTwo = "";
+                }
+
+            });
         });
-    });
-    var performOperation = function (operation) {
+    }
+)
 
-        };
+();
 
-})
-
-    /*  var clickOne=Array.from(document.querySelectorAll("button")).forEach(function ($btn, index) {
-          $btn.addEventListener("click", function () {
-              document.getElementById("img-" + index).src = randomCard[index];
-              i = randomCard[index];
-              indexI=index;
-          });
+/*  var clickOne=Array.from(document.querySelectorAll("button")).forEach(function ($btn, index) {
+      $btn.addEventListener("click", function () {
+          document.getElementById("img-" + index).src = randomCard[index];
+          i = randomCard[index];
+          indexI=index;
       });
+  });
 
 
-      Array.from(document.querySelectorAll("button")).forEach(function ($btn, index) {
-          $btn.addEventListener("click",clickOne, function () {
-              document.getElementById("img-" + index).src = randomCard[index];
-              j = randomCard[index];
-              indexJ=index;
-          });
+  Array.from(document.querySelectorAll("button")).forEach(function ($btn, index) {
+      $btn.addEventListener("click",clickOne, function () {
+          document.getElementById("img-" + index).src = randomCard[index];
+          j = randomCard[index];
+          indexJ=index;
       });
+  });
 
-      if (i !== j){document.getElementById("img-" + indexI).src = "images/pokeball.png";
-          document.getElementById("img-" + indexJ).src = "images/pokeball.png";}
+  if (i !== j){document.getElementById("img-" + indexI).src = "images/pokeball.png";
+      document.getElementById("img-" + indexJ).src = "images/pokeball.png";}
 
-  })*/
+})*/
 
