12cbd37d1ae5d06e1301c7f462f6dbd5592fb56b
Lea Dalibard <leadalibard@gmail.com>
2020-09-11 15:08:35 +0200

    Registering tips from Awet

--- script.js
+++ script.js
@@ -26,21 +26,19 @@
 
         /*------------Turning images-----------------*/
 
-        var count = 0;
+        var count = 1;
         Array.from(document.querySelectorAll("button")).forEach(function ($btn, index) {
             $btn.addEventListener("click", function () {
-                count = count + 1;
                 document.getElementById("img-" + index).src = randomCard[index];
-                if (count === 2) {
+                if (count <2) {
                     function fixCardTwo() {
-                        var cardTwo = randomCard[index]
-                        var indexCardTwo=index
-                        console.log(cardTwo);
+                        var cardTwo = randomCard[index];
+                        var indexCardTwo=index;
                         //count = 0;
                     }
                 } else {
                     function fixCardOne() {
-                        var cardOne = randomCard[index]
+                        var cardOne = randomCard[index];
                         var indexCardOne=index
                     };
                 }
@@ -95,4 +93,18 @@
     }
 )
 */
-
+ /* 1 get the card //
+ * 2 loop over them and listen for event => flipping function, get the id of the card
+ * 3 create array to check if the same
+ * 4 if card1 != card2 turn back else stay
+ * 5 counter=1
+ * 6 src images
+ * 7 creating the flipping function with the parameter card-id
+ * 8 in Flipping function => get the card by id// let card = document.getElementById(${imageId}) (can put parameter of the function)
+ * 9 generate random index => get random images (srcimg[randomIndex])
+ * 10 push random generate images to check array
+ * 11 if counter<2 firstcard= display random image, flip it counter++ => and remove from random sources images
+ * 12 else if firstcard.src= secondcard.src then display the second
+ * 13 else flip both counter-- + firstcard=""+ checkarray=""
+ *
+ * */
