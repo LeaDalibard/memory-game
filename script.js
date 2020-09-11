3680ac89544593e498971fe748b828d12c046f84
Lea Dalibard <leadalibard@gmail.com>
2020-09-11 09:44:32 +0200

    Random array created for images with shuffle function

--- script.js
+++ script.js
@@ -9,24 +9,40 @@
         "images/pikachu.png"
     ];
 
-    const randomArray = []
-    for (let i = 0; i < 11; i++) {
-        var randomImg=
-        randomArray.push(Math.floor(Math.random() * 11))
-    }
-    var x = document.getElementsByTagName("td");
-    for (let i = 0; i < length; i++) {
-        x[i].innerHTML = randomArray[i];
-    }
 
-    x.forEach(index=>  x[index].innerHTML = randomArray[index]);
 
-        Array.from(document.querySelectorAll("button")).forEach(function ($btn, index) {
-            $btn.addEventListener("click", function () {
-                var random = Math.floor(Math.random() * 5);
-                document.getElementById("img-"+ index).src = pokemon[random];
-            });
+    var card = pokemon.concat(pokemon);
+
+    function shuffle(a) {
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
+    var randomCard= shuffle(card);
+    console.log(randomCard);
+
+    /* for (let i = 0; i < 11; i++) {
+         var randomImg=
+         randomArray.push(Math.floor(Math.random() * 11))
+     }
+     var x = document.getElementsByTagName("td");
+     for (let i = 0; i < length; i++) {
+         x[i].innerHTML = randomArray[i];
+     }
+
+     x.forEach(index=>  x[index].innerHTML = randomArray[index]);*/
+
+    Array.from(document.querySelectorAll("button")).forEach(function ($btn, index) {
+        $btn.addEventListener("click", function () {
+            var random = Math.floor(Math.random() * 5);
+            document.getElementById("img-" + index).src = pokemon[random];
         });
+    });
 
 })
 
